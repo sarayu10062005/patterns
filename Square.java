a2e17a248d9564e4c42a8a5dd57c16ef649c40a0
sarayu10062005 <srijasarayu@gmail.com>
2025-01-26 04:33:14 -0800

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,15 @@
+public class Square {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.println("X  #  X  #  X");
+                System.out.println("#  #  #  #  #");
+            }
+        }
+        for (int i = 0; i < 1; i++) {
+            System.out.println("X  #  X  #  X");
+        }
+    }
+}
+
+
